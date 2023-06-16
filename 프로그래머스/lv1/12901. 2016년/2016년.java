bae8382823d2db5e57f5afd2c7a6d4af33a9da9b
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2023-06-16 14:49:49 +0900

    [level 1] Title: 2016년, Time: 1.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,35 @@
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        int dayOfWeekNumber = dayOfWeek.getValue();
+
+        switch(dayOfWeekNumber){
+            case 1 :
+                answer = "MON";
+                break;
+            case 2 :
+                answer = "TUE";
+                break;
+            case 3 :
+                answer = "WED";
+                break;
+            case 4 :
+                answer = "THU";
+                break;
+            case 5 :
+                answer = "FRI";
+                break;
+            case 6:
+                answer = "SAT";
+                break;
+            case 7 :
+                answer = "SUN";
+        }
+        return answer;
+    }
+}
