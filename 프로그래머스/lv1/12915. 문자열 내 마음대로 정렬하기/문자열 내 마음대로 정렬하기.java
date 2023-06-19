f8f41aa13d7c9e0d9ed532cf9ad20e3fad69c449
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2023-06-19 17:18:39 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 15.01 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -4,29 +4,17 @@ class Solution {
     public String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
 
-        // bce, bcd, dx
-//        String[] sample = {"bce", "bcd", "dx"};
-//        Arrays.sort(sample);
-//        System.out.println(Arrays.toString(sample));
-
-        //2.
-        // 각 단어마다 n번째부터 자른 값을 넣어서 배열에 삽입
-        ArrayList <String> arr = new ArrayList<>();
-        for(int i = 0; i < strings.length; i++){
-            arr.add(strings[i].charAt(n)+ strings[i]);
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++) {
+            arr.add(strings[i].charAt(n) + strings[i]);
             // arr = {"babc", "bab"}
         }
-        String[] arr2 = new String[arr.size()];
-        for(int i = 0; i < arr2.length; i++){
-            arr2[i] = arr.get(i);
+        Collections.sort(arr);
+
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).substring(1);
         }
-
-        Arrays.sort(arr2);
-
-        for (int i = 0; i < arr2.length; i++) {
-            answer[i] = arr2[i].substring(1);
-        }
-
+        
         return answer;
     }
 }
