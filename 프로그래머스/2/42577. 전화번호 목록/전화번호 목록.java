61715f918339ee90d0ab69b6f086fad61dd27394
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2025-03-26 10:33:13 +0900

    [level 2] Title: 전화번호 목록, Time: 342.48 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
+/* 풀이 방법 1.
+1. 정렬을 이용해서 1 - 12 - 123 앞의 숫자가 작은 순서대로 정렬(String)하기 -> 문자열이기에 가능
+2. 정렬 했을 경우 앞의 값이 뒤에 값에 포함되어있으면 접두어 ok(false), 아니면 no(true)
+
+   풀이 방법 2.
+1. HashSet을 이용해서 첫번째값을 가지고 다음 값을에 sustring 후 Contain 포함되어 있는지 확인하기
+*/
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < phone_book.length; i++)
-            map.put(phone_book[i], i);
-
-        for (int i = 0; i < phone_book.length; i++)
-            for (int j = 0; j < phone_book[i].length(); j++)
-                if (map.containsKey(phone_book[i].substring(0, j)))
-                    return false;
-
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length-1; i++){
+           if(phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
         return true;
     }
 }
