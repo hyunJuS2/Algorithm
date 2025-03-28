84a011670871c87cfa300d1331294494d1937f7d
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2025-03-28 15:38:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 29.70 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,32 +1,24 @@
-/* 풀이 방법 1.
-1. 스택에 값을 넣음 
-- 비어있지 않는경우 ppek 후 현재와 비교, 
-- 같으면 값을 기존의 값을 pop 후 push,
-- 다르면 그대로 push]
+// List를 사용해서 이전 값이랑 비교해서 다르면 add
 
-   풀이 방법 2.
-1. Stack 대신 Dequeue를 사용해 보자 (실전에서는 stack을 잘 사용하지 않음)
-
-*/
 import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
         int[] answer = {};
         
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         
-        for(int i : arr){
-            if(stack.isEmpty() || stack.peek() != i) {stack.push(i);}
+        for(int i = 0; i < arr.length; i++){
+            if(list.isEmpty() || arr[i] != arr[i-1]){
+                list.add(arr[i]);
+            }
         }
-        // stack -> List 로 복사(순서 유지)
-        List<Integer> list = new ArrayList<>(stack);
-        // List -> int[]
-        answer = new int[list.size()];
-        for(int j = 0; j <list.size(); j++){
+        
+        answer =  new int[list.size()];
+        for(int j = 0; j < list.size(); j++){
             answer[j] = list.get(j);
         }
-        
+
         return answer;
     }
 }
