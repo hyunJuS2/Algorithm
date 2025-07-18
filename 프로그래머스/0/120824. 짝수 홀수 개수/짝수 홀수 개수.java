12f9d661b3bd57b94a528d4dcc7067561f9b57f2
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2025-07-18 20:11:42 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        
+        for(int num : num_list){
+            if(num % 2 == 1){
+                odd++;
+            }
+            else even++;
+        }
+        return new int[]{even, odd};
+    }
+}
