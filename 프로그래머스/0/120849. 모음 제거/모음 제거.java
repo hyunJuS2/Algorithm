a338d9404cd416dcb50c9a84fd3f2bee8d974d1e
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2025-07-21 15:55:04 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -4,3 +4,17 @@ class Solution {
    
     }
 }
+// class Solution {
+//     public String solution(String my_string) {
+//         String answer = "";
+//         StringBuffer sb = new StringBuffer();
+//         for(char c : my_string.toCharArray()){
+//             if(c!= 'a' && c!= 'e' && c!= 'i' && c!='o'&& c!='u'){
+//                 sb.append(c);
+//             }
+//         }
+//         answer = sb.toString();
+
+//         return answer;
+//     }
+// }
